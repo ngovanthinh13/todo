329e2a4ebf1cce64f2721c1a5d3bfc75c36a69ad
unknown <luonchomotdieu@gmail.com>
2020-09-29 23:33:56 +0700

    Updates

---
+++ precache-manifest.b2bfc1cdeb71ee83720209207cec8dcc.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "3716e021d0011d9ca2bf",
+    "url": "/todo/static/css/main.733a4f10.chunk.css"
+  },
+  {
+    "revision": "3716e021d0011d9ca2bf",
+    "url": "/todo/static/js/main.c42de464.chunk.js"
+  },
+  {
+    "revision": "3ce2d46cb5510c372f4d",
+    "url": "/todo/static/js/runtime~main.7a2dc5ec.js"
+  },
+  {
+    "revision": "908118056d67433126b9",
+    "url": "/todo/static/js/2.1ef49e21.chunk.js"
+  },
+  {
+    "revision": "fbe17b86975396379633e7c08ed979e0",
+    "url": "/todo/index.html"
+  }
+];
